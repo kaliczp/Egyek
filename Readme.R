d8ca56a25407d6cf543e157c6418fe44c5dfa76b
Péter Kalicz <kalicz.peter@uni-sopron.hu>
2021-11-10 19:52:25 +0100

    Filter out duplicated temperature

--- Readme.R
+++ Readme.R
@@ -14,3 +14,14 @@ hom <- rbind(hom, ttraw[ttraw$parameter == "Levegőhőmérséklet",])
 library(xts)
 csap.xts <- xts(csap$value, as.POSIXct(csap$date))
 hom.xts <- xts(hom$value, as.POSIXct(hom$date))
+## Duplák, amelyből az első adathiány
+dupla <- duplicated(index(hom.xts))
+dupla <- c(dupla[-1],FALSE)
+dupla.nr <- which(dupla)
+homdup.xts <- hom.xts[dupla.nr]
+homdup.xts[is.na(homdup.xts)]
+nadupla.nr <- dupla.nr[is.na(homdup.xts)]
+hom.xts <- hom.xts[-nadupla.nr]
+## Sima duplák
+dupla <- duplicated(index(hom.xts))
+hom.xts <- hom.xts[!dupla]
