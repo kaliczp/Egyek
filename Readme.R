f1a53272f96f27bb974df09f9870a16b09ce0dd2
Péter Kalicz <kalicz.peter@uni-sopron.hu>
2021-11-10 18:47:19 +0100

    Read in based on for loop
    Now filenames is read and a for loop pastes both temperature and precipitation time-series.

--- Readme.R
+++ Readme.R
@@ -1,17 +1,16 @@
-rawjunaug <- read.csv("Egyekjun1aug1.csv")
-rawaugokt <- read.csv("Egyekaug1okt10.csv")
-
+## Fájlnevek beolvasása
+filenames <- dir(patt = "csv")
+ttraw <- read.csv(filenames[1])
 ## Csapadék
-csap <- rawjunaug[rawjunaug$parameter == "Csapadék",]
-ttcsap <- rawaugokt[rawaugokt$parameter == "Csapadék",]
-csap <- rbind(csap, ttcsap[49:nrow(ttcsap),])
+csap <- ttraw[ttraw$parameter == "Csapadék",]
+## Hőmérséklet
+hom <- ttraw[ttraw$parameter == "Levegőhőmérséklet",]
+## Az összes fájlra
+for(tti in 2:length(filenames)) {
+ttraw <- read.csv(filenames[tti])
+csap <- rbind(csap, ttraw[ttraw$parameter == "Csapadék",])
+hom <- rbind(hom, ttraw[ttraw$parameter == "Levegőhőmérséklet",])
+}
 library(xts)
 csap.xts <- xts(csap$value, as.POSIXct(csap$date))
-
-## Hőmérséklet
-hom <- rawjunaug[rawjunaug$parameter == "Levegőhőmérséklet",]
-tthom <- rawaugokt[rawaugokt$parameter == "Levegőhőmérséklet",]
-hom <- rbind(hom, tthom[49:nrow(tthom),])
 hom.xts <- xts(hom$value, as.POSIXct(hom$date))
-plot(hom.xts)
-raw2020novdec <- read.csv("Egyek2020nov1dec31.csv")
